3894668b56a979420204e9ef7529c4d02c82217c
SHAGE-MD V3 <PRABATHbot@outlook.com>
2024-09-20 18:27:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",
+SESSION_ID: process.env.SESSION_ID || "gfUj0YbY#Mrs9WBTYD8d4GiE3wuLc8NSmnOK0GjuPtjb4n9sKYAQ",
+MONGODB: process.env.MONGODB || "mongodb+srv://mohsin:mohsin@cluster0.iauaztt.mongodb.net/?retryWrites=true&w=majority",
 };
